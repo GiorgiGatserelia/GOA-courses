2b9df9a5be1344499e4ea7b12697f57405e7b213
GiorgiGatserelia <georgebasishvili1@gmail.com>
2024-02-29 00:18:03 +0400

    second commit

---
+++ day3/homework01.py
@@ -0,0 +1,10 @@
+mother = "natia"
+father = "beqa"
+sister = "lile"
+younger_sister = "tamari"
+
+print(mother + " " + father + " " + sister + " " + younger_sister)
+print(mother)
+print(father)
+print(sister)
+print(younger_sister)
---
+++ day4/homework 4.py
@@ -0,0 +1,5 @@
+name = input("please enter your name:")
+lastname = input("please enter your lastname:")
+age = input("please enter your age:")
+
+print(" my name is " + name + " my lastname is " + lastname + " my age is " + str(age))
