530da31bbc685ea58e6f79afa5e9657404ad409c
GiorgiGatserelia <georgebasishvili1@gmail.com>
2024-03-23 21:49:42 +0400

    seventh commit

---
+++ day7/homework7.py
@@ -0,0 +1,42 @@
+my_string = input("Write a string: ")
+my_integer = int(input("Write an integer: "))
+my_float = float(input("Write a float: "))
+my_bool = float(input("Write a Something"))
+
+the_bool = 56
+correct_bool = the_bool > my_bool
+
+print(type(my_string), my_string)
+print(type(my_integer), my_integer)
+print(type(my_float), my_float)
+print(type(my_bool), correct_bool)
+
+
+#davaleba 2
+
+
+
+print( 77 > 43 )
+print( 81 < 90 )
+print( 1  < 100 )
+print( 12  > 11 )
+print( 45 < 48 )
+print( 3 > 1 )
+print( 51 < 52)
+print( 76 < 90)
+
+# davaleba 3
+
+print( False and False)
+print( False and True)
+print(True and False)
+print( True and True)
+
+# and operatori
+
+print(True or False)
+print(False or True)
+print(True or True)
+print(False or False)
+
+# or operatori
